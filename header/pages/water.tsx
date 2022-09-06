0ffb554c4c3f68ff7828cb72c568ece436a15d93
Mohammed amine Ajdahim <52434450+AmineAjdahim@users.noreply.github.com>
2022-09-06 00:43:15 +0000

    water animation

---
+++ header/pages/water.tsx
@@ -0,0 +1,12 @@
+import styles from "../styles/water.module.css"
+export default function Water(){
+    return (
+        <div className="bd h-screen flex justify-center items-center bg-black">
+            <div className={`${styles.cont} `}>
+            <h2 >Water</h2>
+            <h2>Water</h2>
+            </div>
+        </div>
+    )
+    
+}
